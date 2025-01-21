731ef655ba2629d86ae5a2d91ecae766bb0970c5
Ubermench2008 <i.danilov@g.nsu.ru>
2025-01-21 10:50:14 +0900

    Update WavWriter.cpp

--- src/WavWriter.cpp
+++ src/WavWriter.cpp
@@ -2,9 +2,10 @@
 
 WavWriter::WavWriter(std::string fileName): filename(fileName) {}
 
-void WavWriter::write(std::vector<short> data, wav_header header) {
+void WavWriter::write(std::vector<short> data, Wav_header header) {
     header.subchunk2_id[0] = 'd'; header.subchunk2_id[1] = 'a'; header.subchunk2_id[2] = 't'; header.subchunk2_id[3] = 'a';
     header.subchunk2_size = data.size() * 2;
+    
     std::ofstream file(filename, std::ios::binary);
     file.write((char *)&header, sizeof(header));
     file.write((char *)data.data(), data.size());
