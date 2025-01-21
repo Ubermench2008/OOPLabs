224ba51bbfc5187bcbb915127359fa7dfbaf65fb
Ubermench2008 <i.danilov@g.nsu.ru>
2025-01-21 10:20:41 +0900

    Update WavParser.cpp

--- src/WavParser.cpp
+++ src/WavParser.cpp
@@ -3,17 +3,15 @@
 
 WavParser::WavParser(std::string fileName): fileName(fileName) {}
 
-WavParser::WavParser(std::string fileName): fileName(fileName) {}
-
 std::vector<short int> WavParser::parse() {
     std::ifstream file(fileName, std::ios::binary); // читаем файл
     if(!file.is_open()) {
         throw std::runtime_error("Не удалось открыть файл: " + fileName);
     }
 
-    header = new wav_header;
+    header = new Wav_header;
 
-    file.read(reinterpret_cast<char*>(header), sizeof(wav_header));
+    file.read(reinterpret_cast<char*>(header), sizeof(Wav_header));
 
     char* junk = new char[header->subchunk2_size + 4];
     file.read(junk, sizeof(char) * (header->subchunk2_size + 4));
